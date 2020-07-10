a6d61c7b545d048ea87e4dde471f63ed54b6a576
Andrii Hromov <andrii.hromov@gmail.com>
2020-07-10 11:05:54 +0300

    Fix for H2

--- hello-spring-data-jdbc/src/main/resources/db/migration/V1__Create_initial_schema.sql
+++ hello-spring-data-jdbc/src/main/resources/db/migration/V1__Create_initial_schema.sql
@@ -1,12 +1,12 @@
-CREATE TABLE `accounts`
+CREATE TABLE accounts
 (
-    `id` INTEGER PRIMARY KEY,
-    `first_name` VARCHAR(64),
-    `last_name` VARCHAR(64),
-    `email` VARCHAR(64) UNIQUE,
-    `birthday` DATE,
-    `creation_time` TIMESTAMP,
-    `balance` DECIMAL(11, 2)
+    id BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR(64),
+    last_name VARCHAR(64),
+    email VARCHAR(64) UNIQUE,
+    birthday DATE,
+    creation_time TIMESTAMP,
+    balance DECIMAL(11, 2)
 );
 
 
